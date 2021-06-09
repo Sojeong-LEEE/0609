bd8c6e7ee58d158a16519ddbc12a9f321497e324
sojeong <lsojxx@gmail.com>
2021-06-09 13:21:04 +0900

    0609
    0609

---
+++ script.js
@@ -0,0 +1,13 @@
+
+
+$(function(){
+
+    var $title = $('h1');
+    var $button = $('button');
+    console.log($title);
+    console.log($button);
+    
+    $title.click(function(){
+        console.log('>>> click >>>')
+    })
+})
